e80d9941e7bc370a955c7ac5c44cdd995a9987bb
GitHub GraphicYan <GitHub yanzhang.cg@gmail.com>
2022-12-04 22:43:30 +0800

    [update] my init version

---
+++ source/about/index.md
@@ -0,0 +1,54 @@
+---
+title: 个人简介
+layout: about
+---
+
+ <center>
+     <h1>张延</h1>
+     <div>
+         <span>
+             <img src="assets/phone-solid.svg" width="18px">
+             18252033833
+         </span>
+         ·
+         <span>
+             <img src="assets/envelope-solid.svg" width="18px">
+             yanzhang.cg@gmail.com
+         </span>
+         ·
+         <span>
+             <img src="assets/github-brands.svg" width="18px">
+             <a href="https://github.com/GraphicYan">GraphicYan</a>
+         </span>
+         ·
+         <span>
+             <img src="assets/rss-solid.svg" width="18px">
+             <a href="https://graphicyan.github.io/">My Blog</a>
+         </span>
+     </div>
+ </center>
+
+
+ ## <img src="assets/info-circle-solid.svg" width="30px"> 个人信息 
+
+ - 男，1995 年8月生
+ - 兴趣领域：渲染、仿真、游戏引擎。
+
+## <img src="assets/graduation-cap-solid.svg" width="30px"> 教育经历
+
+- **硕士，南京大学，视觉传感与图像实验室，2017.9~2020.6**
+  - TOP高校创新科技大赛AI赛季总冠军（队长）
+  - 一等学业奖学金、院研究生会主席
+  - 发表B类会议论文一篇（Pacific Graphics）、专利一篇
+- **学士，南京大学，电子信息与工程学院，2013.9~2017.6**
+  - 全国大学生电子设计竞赛Intel杯嵌入式邀请赛一等奖（队长）
+  - 南京大学优秀毕业生、优秀共青团干部、保送研究生
+  - CET6：531，IELTS：6.5
+
+## <img src="assets/briefcase-solid.svg" width="30px"> 工作经历
+
+- **淘宝（中国）软件有限公司，大淘宝技术部，技术-算法-应用算法，2020.6~至今**
+
+  ​	全流程参与团队**云渲染**引擎的开发迭代、渲染质量保证和资产数据转换等工作。其中，主要负责了自研GPU离线渲染引擎的**材质**模块开发、**渲染算法**优化、场景**数据转换**及其与VRay**渲染效果**对齐的任务；此外，负责了模型处理链路（千万级）的**材质转换**算法节点及渲染**资产优化**的工作；同时，负责了基于**虚幻引擎**及**USD**的规模化场景生产与渲染项目，开发USD2UE插件以及UE云渲染服务。
+
+## <img src="assets/project-diagram-solid.svg" width="30px">
---
+++ source/convert_images.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import os
+import re
+import time
+
+prefix = 'https://intranetproxy.alipay.com/skylark'
+postfix = '#'
+local_img_folder = 'assets/post_images/'
+md_path  = r'/Users/yan/Blog/GithubHexoBlog/source/_posts/'
+local_img_prefix = r'/Users/yan/Blog/GithubHexoBlog/public/'
+
+def gen_image_name_from_idx(idx, img_folder_path, postfix = 'png'):
+    if idx < 10:
+        name = '00' + str(idx)
+    elif idx < 100:
+        name = '0' + str(idx)
+    elif idx < 1000:
+        name = str(idx)
+    res = img_folder_path + '/image_' + name + '.' + postfix
+    print('[gen image name] ' + res)
+    return res
+
+
+def handle_md_images(md_name):
+    md_file_path = md_path + md_name + '.md'
+    img_path_folder = local_img_folder + md_name
+    if not os.path.exists(local_img_prefix + img_path_folder):
+        print('[mkdir] : ' + local_img_prefix + img_path_folder)
+        os.mkdir(local_img_prefix + img_path_folder)
+    img_idx = 0
+    try:
+        if md_file_path.split('.')[-1] == 'md':
+            split_file_name = md_file_path.split('.')
+            split_file_name[-2] += '_converted'
+            copy_md_file_path = '.'.join(split_file_name)
+            write_lines = []
+            img_idx = 0
+            # 打开md文件然后进行替换
+            with open(md_file_path, 'r', encoding='utf8') as fr:
+                    lines = fr.readlines()
+                    for line in lines:
+                        _line = line          
+                        if prefix in line:
+                            search_from = 0
+                            while search_from < len(line):
+                                # find image ref url
+                                start_idx = line.find(prefix,search_from)
+                                end_idx = line.find(postfix, search_from)
+                                if start_idx < 0 or end_idx < 0:
+                                    break
+                                img_url = line[start_idx:end_idx]
+                                search_from = end_idx + 1
+                                print(img_url)
+                                
+                                # download image
+                                img_postfix = img_url.split('.')[-1]
+                                local_img =  gen_image_name_from_idx(img_idx,img_path_folder,img_postfix)
+                                img_idx += 1
+                                local_img_path = local_img_prefix + local_img
+                                print('[download img] from URL: {' + img_url+ '} to local path: {' + local_img + '}')
+                                cmd_wget = 'wget -O ' + local_img_path + ' ' + img_url
+                                print(cmd_wget)
+                                os.system(cmd_wget)
+
+                                # replace md content
+                                _line = _line.replace(img_url,local_img,1)
+                        write_lines.append(_line)
+
+            with open(copy_md_file_path, 'w', encoding='utf8') as fw:
+                    fw.writelines(write_lines)  # 新文件一次性写入原文件内容
+                    # fw.flush()
+
+            # 备份原文件
+            os.rename(md_file_path, md_file_path + '.bak')
+            # 重命名新文件名为原文件名
+            os.rename(copy_md_file_path, md_file_path)
+            print(f'{md_file_path} done...')
+            time.sleep(0.5)
+    except FileNotFoundError as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    md_name = r'test'
+    for file in os.listdir(md_path):
+        if file.endswith('.md'):
+            md_name = file.split('.')[:-1]
+            handle_md_images('.'.join(md_name))
